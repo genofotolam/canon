61f5d885d6b8b4b5f93ce6f9cb94dbfc58926a0b
genofotolam <genofotolam@outlook.com>
2024-08-15 22:54:44 +0300

    Add files via upload

---
+++ create8.bat
@@ -0,0 +1,14 @@
+@echo off
+set GITHUB_TOKEN=github_pat_11BKR7C6Q0ZxjuoSoAHLwB
+set GITHUB_TOKEN2=_1mJ9gvIdGeGCqoeb2W12zZysQac1HcJito1TA52aVmm3GAYKO64oDSS8yrI
+set REPO_OWNER=genofotolam
+set REPO_NAME=canon
+
+set WORKFLOW_FILE=revisio8.yml
+
+curl -X POST -H "Accept: application/vnd.github.v3+json" ^
+-H "Authorization: token %GITHUB_TOKEN%%GITHUB_TOKEN2%" ^
+https://api.github.com/repos/%REPO_OWNER%/%REPO_NAME%/actions/workflows/%WORKFLOW_FILE%/dispatches ^
+-d "{\"ref\":\"main\"}"
+
+pause
